579195f94ca0f409de8d32c3843c5d98d1cd63dc
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2023-06-26 15:06:34 -0300

    criei o exercícios 8 9 e 10

---
+++ exercicios/aula 11/ex08.js
@@ -0,0 +1,8 @@
+var vel = 34
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) { //condição simples
+    console.log(`Você utrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança`)
