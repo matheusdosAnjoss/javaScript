c640c80ad7beba596f9cb04fe73084198386a2b1
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2023-06-30 20:50:22 -0300

    att

---
+++ exercicios/aula 14 ex/script.js
@@ -0,0 +1,8 @@
+
+function calcular() {
+    var inicio = document.getElementById('ini')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('pas')
+    var res = document.getElementById('res')
+    res.innerHTML = (`Vc escolheu o número ${ini} ${fim} ${pas}`)
+}
