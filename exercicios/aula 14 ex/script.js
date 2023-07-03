df399a8ecb4191d9d837d0022cdfb8915713dcd0
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2023-07-03 14:19:12 -0300

    ex 14

--- exercicios/aula 14 ex/script.js
+++ exercicios/aula 14 ex/script.js
@@ -4,5 +4,32 @@ function calcular() {
     var fim = document.getElementById('fim')
     var passo = document.getElementById('pas')
     var res = document.getElementById('res')
-    res.innerHTML = (`Vc escolheu o número ${ini} ${fim} ${pas}`)
+   
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ){
+        res.innerHTML = 'Impossível contar'
+        window.alert('[ERRO] Faltam dados')     
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if (i < f) { //Contagem crescente
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+            
+        } else { //Contagem regressiva
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        }
+                res.innerHTML += `\u{1f3c1}`
+    } 
 }
